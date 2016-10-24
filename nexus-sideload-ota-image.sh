7a34819b9841d21c70eaf6c874cbc8e909d5562a
jvanmeter <jonathan.vanmeter@gmail.com>
2016-10-24 00:55:34 -0400

    Update nexus-sideload-ota-image.sh

--- nexus-sideload-ota-image.sh
+++ nexus-sideload-ota-image.sh
@@ -4,35 +4,31 @@
 # When sideloading newer versions of Android (e.g. 5.1 and later), ADB version 1.0.32+ is required.
 
 # Step one - find the URL to the OTA update for phone.  Verify the version matches current Android version and phone vendor/carrier, etc.
+# Google publishes OTA images to the following webpage:  https://developers.google.com/android/nexus/ota
 
-# Check adb version
+# Check adb version 
 adb version
 
-# If adb is version 1.0.31 or older, follow the update process below:
-wget -O - https://skia.googlesource.com/skia/+archive/cd048d18e0b81338c1a04b9749a00444597df394/platform_tools/android/bin/linux.tar.gz | tar -zxvf - adb
-
-# Backup current adb version
-sudo mv /usr/bin/adb /usr/bin/adb.old
-sudo mv adb /usr/bin/adb
-sudo chmod +x /usr/bin/adb
-
-# Get 32bit compatibility libraries if using a 64bit Linux environment and the adb executable is not recognized.
-sudo apt-get install libc6:i386 libstdc++6:i386
+# If adb is not found or older than 1.0.31 install the latest packages
+# Fedora 24:
+# sudo dnf -y install android-tools 
 
 # Verify new adb can be executed and check version
-adb version
 adb devices
 
 # Boot phone into recovery
-adb reboot recovery
-
 # Alternatively the phone specific bootloader button combination can be used
 # Nexus 5 - Volume Up + Volume Down + Power
 # Nexus 6/7/9 - Volume Down for a few seconds, then press and hold Power at the same time
+adb reboot recovery
 
 # At the "Broken Android" with exclamation point for Nexus 5 hold power + tap Volume Up
 # Highlight "Apply update from ADB" with vol keys and press power to select
+
+# It is common to receive the message "error: insufficent permissions for device"
+# Restarting the adb service will clear this error
+adb kill-server;adb start-server
 adb sideload <OTA file name>
 
-# Be patient, the sideload update process takes 40+ minutes to complete.
+# Be patient, the sideload update process can take 40+ minutes to complete for Android 5.x & 6.x.
 # An additional 20+ minutes needed for app cache updates; dependent on number of apps installed.
